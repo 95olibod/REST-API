806ea1531b13134bded677fd49bd393619ddeec3
Olivia Bodén <Olivia.annemaria@hotmail.com>
2021-08-18 15:00:39 +0200

    GET method works using router file

---
+++ animals/router.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+
+//inmemory db
+const animals = [{
+    id: 0,
+    name: 'Dodger',
+    animalType: 'Dog',
+    otherInfo: 'Bichon frisé'
+}];
+
+//create object router
+const router = express.Router();
+
+/**
+ * Responds with all animals
+ * @param {express.Request} req
+ * @param {express.Response } res
+ * @param {express.NextFunction } req
+ */
+router.get('/api/animals', (req, res, next) => {
+    res.json(animals);
+});
+
+router.post('/api/animals');
+router.put('/api/animals');
+router.delete('/api/animals');
+
+//export the object router
+module.exports = router;
+
+
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //adding this so i can reach express and otehr implementations
 const express= require('express');
-const animalsRouter = require('./router');
+const animalsRouter = require('./animals/router');
 
 //Creating a server app
 const app = express();
@@ -8,6 +8,10 @@ const app = express();
 //Parsing the incoming JSON
 app.use(express.json());
 
+//Adding resources
+app.use(animalsRouter);
+
+// Start the server
 app.listen(3000, () => {
     console.log('sever is running at http://localhost:3000')
-})
+}) 
